3176c7997e033abd39b89cb8d5a09b4a0eaaafc7
meokisama <hoangdinhsang123@gmail.com>
2021-11-01 01:12:59 +0700

    Update v1.0.5

--- wp-login.php
+++ wp-login.php
@@ -113,7 +113,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/blog\/wp-admin\/admin-ajax.php"}};
 <script type="text/javascript" src="/alonelycomet/wp-includes/js/wp-util.min.js?ver=5.8.1" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /*  */
-var userProfileL10n = {"user_id":"0","nonce":"9e40ab6766"};
+var userProfileL10n = {"user_id":"0","nonce":"de602e7471"};
 /*  */
 </script>
 <script type="text/javascript" id="user-profile-js-translations">
